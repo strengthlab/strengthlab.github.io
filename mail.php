ddb234777ff8d1f738fa667c4a465a5f8890f61e
nbacuez <nbacuez@indeed.com>
2020-07-19 14:14:39 -0500

    starting on online class form

--- mail.php
+++ mail.php
@@ -1,11 +1,17 @@
 <?php
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$formcontent=" From: $name - $email \n Message: $message";
-$recipient = "nicholasbacuez@gmail.com";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+extract($_POST);
+if(isset($_POST['submit'])) {
+    echo "<pre>"; print_r($_POST);
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $formcontent=" From: $name - $email \n Message: $message";
+    $recipient = "nicholasbacuez@gmail.com";
+    $subject = "Contact Form";
+    $mailheader = "From: $email \r\n";
+    mail($recipient, $subject, $formcontent, $mailheader);
+    echo "Thank You!";
+    header('Location:online_training.html');
+}
 ?>
+
